8fd8d222ccc33aee9859bb90037674ceb733b5fb
Odinn30 <masterispower.30@gmail.com>
2026-07-31 20:40:07 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,10 +50,10 @@ RAREZAS = {
 }
 
 CARTAS = [
-    {"id": "c1", "nombre": "Guerrera Novata", "rareza": "B", "foto": "https://i.imgur.com/EJEMPLO1.jpg"},
-    {"id": "c2", "nombre": "Mago del Bosque", "rareza": "A", "foto": "https://i.imgur.com/EJEMPLO2.jpg"},
-    {"id": "c3", "nombre": "Caballero Oscuro", "rareza": "S", "foto": "https://i.imgur.com/EJEMPLO3.jpg"},
-    {"id": "c4", "nombre": "Reina Celestial", "rareza": "SSS", "foto": "https://i.imgur.com/EJEMPLO4.jpg"},
+    {"id": "c1", "nombre": "Egirl Culona", "rareza": "B", "foto": "https://i.postimg.cc/FsPpfjHh/IMG-20260731-200022-446.jpg"},
+    {"id": "c2", "nombre": "Nekotina", "rareza": "A", "foto": "https://i.postimg.cc/dtW678Vw/IMG-20260731-200048-385.jpg"},
+    {"id": "c3", "nombre": "Puta Barata", "rareza": "S", "foto": "https://i.postimg.cc/Nf30gnv2/HMtb-GEibc-AACsx-U.jpg"},
+    {"id": "c4", "nombre": "China Cachonda", "rareza": "SSS", "foto": "https://i.postimg.cc/RVk0MYx6/HOZb3Mma-UAAV2t-K.jpg"},
     {"id": "c5", "nombre": "Deidad Suprema UR", "rareza": "UR", "foto": "https://i.imgur.com/EJEMPLO5.jpg"},
 ]
 
